fc9361b8b03667b423a21f74e37a7f472d3950cd
nwagenbrenner <nwagenbrenner@gmail.com>
2016-06-28 14:17:53 +0000

    fix cleanup script

--- WindNinja-Server/cleanUpOldJobs.sh
+++ WindNinja-Server/cleanUpOldJobs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 
 #delete jobs older than 5 days except the Point Six job
-find /srv/WindNinjaServer/Data/job/* -maxdepth 0 -type d -ctime +5 -name '23becdaadf7c4ec2993497261e63d813' -prune -o -exec rm -rf {} \;
+find /srv/WindNinjaServer/Data/job/* -maxdepth 0 -type d -mtime +5 ! -name '23becdaadf7c4ec2993497261e63d813' -type d -exec rm -rf {} \;
