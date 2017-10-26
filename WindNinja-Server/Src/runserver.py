c7951a8bad055064e1a4b82aeb9a45683d722fc7
fspataro <fspataro@yourdatasmarter.com>
2017-10-26 12:21:27 -0600

    add logging config to runserver

--- WindNinja-Server/Src/runserver.py
+++ WindNinja-Server/Src/runserver.py
@@ -4,6 +4,9 @@ This script runs the windninjaweb application using a development server.
 
 from os import environ
 from windninjaweb.app import app
+import sys
+import logging
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 if __name__ == '__main__':
     #HOST = environ.get('SERVER_HOST', '0.0.0.0')
